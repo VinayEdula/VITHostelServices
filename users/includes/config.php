4207de100bbe2286c40ec7d6894e02e61cf90aef
Edula Vinay Kumar Reddy <vinaykumarreddyedula@gmail.com>
2021-12-01 23:19:42 +0530

    Update config.php

--- users/includes/config.php
+++ users/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost:3399');  //Defining constatnts
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'vhs');
+define('DB_SERVER','remotemysql.com');  //Defining constatnts
+define('DB_USER','eQc0Qn9CdB');
+define('DB_PASS' ,'3PAHTpU9TN');
+define('DB_NAME', 'eQc0Qn9CdB');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME); //function to open a new connection to the MySQL server.
 // Check connection
 if (mysqli_connect_errno()) //Return the last error code for the most recent function call
