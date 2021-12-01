829d5523897a94d44e14311a4ba409178b9b1af0
Edula Vinay Kumar Reddy <vinaykumarreddyedula@gmail.com>
2021-12-01 23:14:45 +0530

    Update config.php

--- VHS admin/Mess admin/include/config.php
+++ VHS admin/Mess admin/include/config.php
@@ -1,6 +1,7 @@
 <?php
 
-$con = mysqli_connect("localhost:3399","root","","cms");
+// $con = mysqli_connect("localhost:3399","root","","cms");
+$con = mysqli_connect("remotemysql.com","eQc0Qn9CdB","3PAHTpU9TN","eQc0Qn9CdB");  //Remote database connection
 // Check connection
 if (mysqli_connect_errno())
 {
